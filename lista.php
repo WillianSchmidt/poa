616a5bbc4d450203a3bcc81f5b3688578f86080b
WillianSchmidt <42277619+WillianSchmidt@users.noreply.github.com>
2018-12-12 20:01:00 -0200

    Parte Final
    Ajuste de listagem de denuncias e dos usuarios na tela

--- lista.php
+++ lista.php
@@ -1,37 +1,231 @@
-<?php
 
-include('database_functions.php');
 
-$pdo = connect_to_database("falacidadebaixa");
 
-$sql = "SELECT * FROM denuncias";
-$result = $pdo->query($sql);
-echo "<!DOCTYPE html><html><body><table border='1'>";
-?>
-<div style="text-align:center;">
-		<?php
-			while ($row = $result->fetch()) {		
-				echo "<h4>Usuario ".$row['nome']."</h4>".
-					 "<h4>Falou : ".($row['comments'] == NULL ? "-" : $row['comments'])."</h4>";
-				echo "----------------------------------------------------------------------------------------------";	 
-					 
-			}
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Fala Cidade Baixa!</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      font: 400 15px Lato, sans-serif;
+      line-height: 1.2;
+      color: #818181;
+  }
+  h2 {
+      font-size: 24px;
+      text-transform: uppercase;
+      color: #303030;
+      font-weight: 600;
+      margin-bottom: 30px;
+  }
+  h4 {
+      font-size: 19px;
+      line-height: 1.500em;
+      color: #303030;
+      font-weight: 400;
+      margin-bottom: 30px;
+  }  
+  .jumbotron {
+      background-color: #3cb371;
+      color: #fff;
+      padding: 100px 25px;
+      font-family: Montserrat, sans-serif;
+  }
+  .container-fluid {
+      padding: 60px 50px;
+  }
+  .bg-grey {
+      background-color: #f6f6f6;
+  }
+  .logo-small {
+      color: #f4511e;
+      font-size: 50px;
+  }
+  .logo {
+      color: #f4511e;
+      font-size: 200px;
+  }
+  .thumbnail {
+      padding: 0 0 15px 0;
+      border: none;
+      border-radius: 0;
+  }
+  .thumbnail img {
+      width: 100%;
+      height: 100%;
+      margin-bottom: 10px;
+  }
+  .carousel-control.right, .carousel-control.left {
+      background-image: none;
+      color: #f4511e;
+  }
+  .carousel-indicators li {
+      border-color: #f4511e;
+  }
+  .carousel-indicators li.active {
+      background-color: #f4511e;
+  }
+  .item h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      font-weight: 400;
+      font-style: italic;
+      margin: 70px 0;
+  }
+  .item span {
+      font-style: normal;
+  }
+  .panel {
+      border: 1px solid #f4511e; 
+      border-radius:0 !important;
+      transition: box-shadow 0.5s;
+  }
+  .panel:hover {
+      box-shadow: 5px 0px 40px rgba(0,0,0, .2);
+  }
+  .panel-footer .btn:hover {
+      border: 1px solid #f4511e;
+      background-color: #fff !important;
+      color: #f4511e;
+  }
+  .panel-heading {
+      color: #fff !important;
+      background-color: #f4511e !important;
+      padding: 25px;
+      border-bottom: 1px solid transparent;
+      border-top-left-radius: 0px;
+      border-top-right-radius: 0px;
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+  }
+  .panel-footer {
+      background-color: white !important;
+  }
+  .panel-footer h3 {
+      font-size: 32px;
+  }
+  .panel-footer h4 {
+      color: #aaa;
+      font-size: 14px;
+  }
+  .panel-footer .btn {
+      margin: 19px 0;
+      background-color: #ff0000;
+      color: #fff;
+  }
+  .navbar {
+      margin-bottom: 0;
+      background-color: #3cb371;
+      z-index: 9999;
+      border: 0;
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #000000 !important;
+      background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color:  !important;
+  }
+  footer .glyphicon {
+      font-size: 20px;
+      margin-bottom: 20px;
+      color: #f4511e;
+  }
+  .slideanim {visibility:hidden;}
+  .slide {
+      animation-name: slide;
+      -webkit-animation-name: slide;
+      animation-duration: 1s;
+      -webkit-animation-duration: 1s;
+      visibility: visible;
+  }
+  @keyframes slide {
+    0% {
+      opacity: 0;
+      transform: translateY(70%);
+    } 
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+  @-webkit-keyframes slide {
+    0% {
+      opacity: 0;
+      -webkit-transform: translateY(70%);
+    } 
+    100% {
+      opacity: 1;
+      -webkit-transform: translateY(0%);
+    }
+  }
+  @media screen and (max-width: 800px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+    .btn-lg {
+        width: 100%;
+        margin-bottom: 35px;
+    }
+  }
+  @media screen and (max-width: 500px) {
+    .logo {
+        font-size: 150px;
+    }
+  }
+  </style>
+</head>
+
+  <body>
+
+
+
+
+
+<form action = "index.html">
+	<div class="jumbotron text-center">
+	  <h1><b>Obrigado por Denúnciar!</b></h1> 
+	  <h4> &nbsp </h4>
+	  
+	  <?php
+		include('database_functions.php');
+		$pdo = connect_to_database("falacidadebaixa");
+		$sql = "SELECT * FROM denuncias";
+		$result = $pdo->query($sql);
 		?>
-</div>
-
-
-
-    <div class="col-sm-7 slideanim" style="text-align:center;">
-	 <form class="myform" action="index.html" method="post">
-      <div class="row">
-        <div class="col-sm-12 form-group"> 
-		  <button class="btn btn-default pull-right" id="enviar" type="submit"><a>Voltar a Inicio</a></button>
+		
+		<div style="text-align:center;">
+				<?php
+					while ($row = $result->fetch()) {		
+						echo "<p><b>Usuario : </b>".$row['nome']."</p>".
+							 "<p><b>Falou : </b>".($row['comments'] == NULL ? "-" : $row['comments'])."</p>";
+						echo "<h4> &nbsp </h4>";	 	 
+					}
+					
+					echo "<button class="."btn btn-default pull-center". "id="."enviar"." type = "."submit"." ><a>Voltar ao In&iacutecio</a></button>";
+				?>
 		</div>
+		
+	  
 	</div>
-	</div>
-</div>
+ </form>
+</body>
 
-
-<?php
-echo "</table></body></html>";
-?>
+</body>
